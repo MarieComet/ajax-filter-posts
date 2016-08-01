2b5c368f3f4b1bab6a0ef5a135faf0da8611ef4e
marie_comet <mariecomet666@gmail.com>
2016-08-01 12:26:38 +0200

    initial

---
+++ ajax-filter-posts.php
@@ -0,0 +1,125 @@
+<?php
+/*
+Plugin Name: Ajax Filter Posts
+Description: Filter posts by taxonomy with ajax, css based on bootsrap but do what you want.
+Version:     1.1
+Author:      Marie Comet
+*/
+class Ajax_Filter_Posts {
+
+	public function __construct(){
+		add_action('muplugins_loaded', array($this, 'init'), 2);
+	}
+	public function init(){
+		//Add Ajax Actions
+		add_action('wp_enqueue_scripts', array( $this, 'enqueue_genre_ajax_scripts' ));
+		add_action('wp_ajax_genre_filter', array( $this, 'ajax_genre_filter'));
+		add_action('wp_ajax_nopriv_genre_filter', array( $this, 'ajax_genre_filter'));
+
+	}
+
+	//EnqueueScripts
+
+	public function enqueue_genre_ajax_scripts() {
+	    wp_register_script( 'genre-ajax-js', plugin_dir_url(__FILE__). 'genre.js', array( 'jquery' ), '', true );
+	    wp_localize_script( 'genre-ajax-js', 'ajax_genre_params', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
+	    wp_enqueue_script( 'genre-ajax-js' );
+		wp_enqueue_style( 'style-css', plugin_dir_url( __FILE__ ) . 'style.css'  );
+	}
+
+	/* Public function get_genre_filters accepts two parameters 
+	*	$post_type :  the post type slug to query
+	* 	$taxo : the taxonomy slug to query
+	*	used for display the filters in your template (for example archive.php) like that :
+	*	$new_posts_filter = new Ajax_Filter_Posts();
+	*	echo $new_posts_filter->get_genre_filters('post', 'category');
+	*/
+	public function get_genre_filters($post_type, $taxo) {
+
+	    $terms = get_terms($taxo);
+	    $filters_html = false;
+	 
+	    if( $terms ):
+	    	$filters_html .= '<div id="genre-filter">';
+	    	$filters_html .= '<input type="hidden" name="post_type" value="'. $post_type .'" id="post_type"/>';
+	    	$filters_html .= '<input type="hidden" name="taxo" value="'. $taxo .'" id="taxo"/>';
+	        foreach( $terms as $term )
+	        {
+	            $term_id = $term->term_id;
+	            $term_name = $term->name;
+	 
+	            $filters_html .= '<a class="term_id_'.$term_id.' btn btn-large"><input type="checkbox" name="filter_genre[]" value="'.$term_id.'" class="input-filter-work">'.$term_name.'</a>';
+	        }
+	        $filters_html .= '<a class="clear-all btn btn-large">Tous</a>';
+	        $filters_html .= '</div><div id="genre-results" class="row row-eq-height no-gutters"></div>';
+	 
+	        return $filters_html;
+	    endif;
+	}
+
+	/* Public function ajax_genre_filters 
+	*	Ajax call construct loop and results
+	*/
+	public function ajax_genre_filter()
+	{
+		$query_data = $_GET;
+
+		$post_type = ($query_data['post_type']) ? $query_data['post_type'] : false;
+		$taxo = ($query_data['taxo']) ? $query_data['taxo'] : false;
+
+		$genre_terms = ($query_data['genres']) ? explode(',',$query_data['genres']) : false;
+
+		$tax_query = ($genre_terms) ? array( array(
+			'taxonomy' => $taxo,
+			'field' => 'id',
+			'terms' => $genre_terms
+		) ) : false;
+		
+		$search_value = ($query_data['search']) ? $query_data['search'] : false;
+		
+		$paged = (isset($query_data['paged']) ) ? intval($query_data['paged']) : 1;
+		
+		$book_args = array(
+			'post_type' => $post_type,
+			's' => $search_value,
+			'posts_per_page' => 8,
+			'tax_query' => $tax_query,
+			'paged' => $paged
+		);
+		$book_loop = new WP_Query($book_args);
+
+		// here place your own loader
+		echo '<div class="ajax-post-loader"><img src="' . plugin_dir_url(__FILE__) . '/space-comet-dark.png" class="comet-dark"/></div>';
+		
+		if( $book_loop->have_posts() ):
+			while( $book_loop->have_posts() ): $book_loop->the_post();
+			setup_postdata($post); ?>
+				<div class="col-xs-12 col-sm-6 col-md-3">
+	        		<div class="work-box">
+	        			<a href="<?php echo get_permalink( $post->ID ); ?>"><span><?php the_title(); ?></span>
+						<?php the_post_thumbnail( 'works-thumb' ); ?>
+						</a>
+					</div>
+		    	</div>
+			<?php endwhile; ?>
+			<?php
+			echo '<div class="genre-filter-navigation col-xs-12 col-sm-12 col-md-12">';
+			        $big = 999999999;
+			        echo paginate_links( array(
+			            'base' => esc_url_raw( str_replace( 999999999, '%#%', remove_query_arg( 'add-to-cart', get_pagenum_link( 999999999, false ) ) ) ),
+			            'format' => '?paged=%#%',
+			            'current' => max( 1, $paged ),
+			            'total' => $book_loop->max_num_pages
+			        ) );
+
+			        echo '</div>';
+			?>
+		<?php else:
+			get_template_part('content-none');
+		endif;
+		wp_reset_postdata();
+		
+		die();
+	}
+}
+new Ajax_Filter_Posts();
---
+++ genre.js
@@ -0,0 +1,94 @@
+//Genre Ajax Filtering
+jQuery(function($)
+{
+    // Get the post_type and taxo on load
+    var post_type = $(this).find('#post_type').val();
+    var taxo = $(this).find('#taxo').val();
+
+    //Load posts on document ready
+    genre_get_posts();
+
+    //If list item is clicked, trigger input change and add css class
+    $('#genre-filter a').live('click', function(){
+        var input = $(this).find('input');
+ 
+        //Check if clear all was clicked
+        if ( $(this).hasClass('clear-all') )
+        {
+            $('#genre-filter a').removeClass('selected').find('input').prop('checked',false); //Clear settings
+            genre_get_posts(); //Load Posts
+        }
+        else if (input.is(':checked'))
+        {
+            input.prop('checked', false);
+            $(this).removeClass('selected');
+        } else {
+            input.prop('checked', true);
+            $(this).addClass('selected');
+        }
+ 
+        input.trigger("change");
+    });
+ 
+    //If input is changed, load posts
+    $('#genre-filter input').live('change', function(){
+        genre_get_posts(); //Load Posts
+    });
+ 
+    //Find Selected Genres
+    function getSelectedGenres()
+    {
+        var genres = []; //Setup empty array
+ 
+        $("#genre-filter a input:checked").each(function() {
+            var val = $(this).val();
+            genres.push(val); //Push value onto array
+        });     
+ 
+        return genres; //Return all of the selected genres in an array
+    }
+ 
+    //If pagination is clicked, load correct posts
+    $('.genre-filter-navigation a').live('click', function(e){
+        e.preventDefault();
+ 
+        var url = $(this).attr('href'); //Grab the URL destination as a string
+        var paged = url.split('&paged='); //Split the string at the occurance of &paged=
+ 
+        genre_get_posts(paged[1]); //Load Posts (feed in paged value)
+    });
+ 
+    //Main ajax function
+    function genre_get_posts(paged)
+    {
+        var paged_value = paged; //Store the paged value if it's being sent through when the function is called
+        var ajax_url = ajax_genre_params.ajax_url; //Get ajax url (added through wp_localize_script)
+ 
+        $.ajax({
+            type: 'GET',
+            url: ajax_url,
+            data: {
+                action: 'genre_filter',
+                post_type: post_type,
+                taxo: taxo,
+                genres: getSelectedGenres, //Get array of values from previous function
+                paged: paged_value //If paged value is being sent through with function call, store here
+            },
+            beforeSend: function ()
+            {
+               $('.ajax-post-loader img').fadeIn("slow");
+            },
+            success: function(data)
+            {
+                $('#genre-results').show("slow").html(data).fadeIn("slow");
+                $('.ajax-post-loader img').fadeOut("slow");
+            },
+            error: function()
+            {
+                                //If an ajax error has occured, do something here...
+                $("#genre-results").html('<p>Rien ici!</p>');
+            }
+        });
+    }
+ 
+});
